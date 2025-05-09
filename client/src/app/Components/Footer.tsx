f60a0ca8b88a9f63056556ea49ddcff670f03e07
AritraChakraborty2003 <aritra.chakraborty203@gmail.com>
2025-05-09 23:28:55 +0530

    Update:Update in code

--- client/src/app/Components/Footer.tsx
+++ client/src/app/Components/Footer.tsx
@@ -48,7 +48,7 @@ const Footer = () => {
             </div>
             <button
               type="submit"
-              className="w-[40%] md:w-[40%] py-3 bg-darkviolet text-white font-medium text-lg md:rounded-none md:rounded-r-full rounded-r-full transition-all hover:bg-[#a95b99]"
+              className="w-[40%] md:w-[40%] py-3 bg-darkviolet text-white font-medium text-md lg:text-lg md:rounded-none md:rounded-r-full rounded-r-full transition-all hover:bg-[#a95b99]"
             >
               Subscribe
             </button>
