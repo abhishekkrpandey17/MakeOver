30053ce208f8348045335c1566cafd44a31d979c
AritraChakraborty2003 <aritra.chakraborty203@gmail.com>
2025-05-12 12:51:15 +0530

    UPDATE:update in code

--- backend/index.js
+++ backend/index.js
@@ -10,8 +10,8 @@ import blogRoutes from "./routes/blogRoutes.js";
 import commentRoutes from "./routes/commentRoutes.js";
 import replyRoutes from "./routes/replyRoutes.js";
 
-import swaggerUi from "swagger-ui-express";
-import { swaggerSpec } from "./swagger.js";
+// import swaggerUi from "swagger-ui-express";
+// import { swaggerSpec } from "./swagger.js";
 
 dotenv.config();
 const app = express();
@@ -20,23 +20,30 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: process.env.NODE_ENV === "production"
-    ? "https://blogs.make-over.in"
-    : "http://localhost:3000",
+  origin:
+    process.env.NODE_ENV === "production"
+      ? "https://blogs.make-over.in"
+      : "http://localhost:3000",
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"]
+  methods: ["GET", "POST", "PUT", "DELETE"],
 };
 
 app.use(cors(corsOptions));
 connectDB();
 
+app.get("/", (req, res) => {
+  res.json({ status: 200, message: "OK" });
+});
+app.get("/api/v1/", (req, res) => {
+  res.json({ status: 200, message: "OK" });
+});
 app.use("/api/v1/admin", adminRoutes);
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/blogs", blogRoutes);
 app.use("/api/v1/comments", commentRoutes);
 app.use("/api/v1/replies", replyRoutes);
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.get("/", (req, res) => {
   res.send("Make_Over Blog Platform API is running");
