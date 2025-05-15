8721cc9a2674b2bc613778dc3afbf6317a3c83b4
AritraChakraborty2003 <aritra.chakraborty203@gmail.com>
2025-05-15 13:23:45 +0530

    FEAT:Developed the admin and author CMS

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,13 +4,22 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 export const registerUser = async (req, res) => {
-  const { email, password, name, gender, preferences } = req.body;
+  const { email, password, name, phone, gender, preferences } = req.body;
+  console.log("Hit");
   try {
     const existingUser = await User.findOne({ email });
-    if (existingUser) return res.status(400).json({ message: "User already exists" });
+    if (existingUser)
+      return res.status(400).json({ message: "User already exists" });
 
     const hash = await bcrypt.hash(password, parseInt(process.env.SALT_ROUNDS));
-    const user = new User({ email, password: hash, name, gender, preferences });
+    const user = new User({
+      email,
+      password: hash,
+      name,
+      gender,
+      phone,
+      preferences,
+    });
     await user.save();
     res.status(201).json({ message: "User registered" });
   } catch (err) {
@@ -27,12 +36,16 @@ export const loginUser = async (req, res) => {
     const match = await bcrypt.compare(password, user.password);
     if (!match) return res.status(401).json({ message: "Invalid credentials" });
 
-    const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "1d" });
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
     res.cookie("token", token, {
       httpOnly: true,
       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       secure: process.env.NODE_ENV === "production",
-      maxAge: 86400000
+      maxAge: 86400000,
     });
     res.json({ message: "Login successful", role: user.role, name: user.name });
   } catch (err) {
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,17 @@
 import express from "express";
-import { registerUser, loginUser, logoutUser } from "../controllers/userController.js";
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+} from "../controllers/userController.js";
+import { verifyUser } from "../middlewares/verifyUser.js";
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
+router.post("/isLogin", verifyUser, (req, res) => {
+  res.json({ status: 200 });
+});
 
 export default router;
